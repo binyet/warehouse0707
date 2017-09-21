38c2e7146786bf2419a806015ecdb5a309e58c8a
binyet <1244257841@qq.com>
2017-09-21 21:32:19 +0800

    停止运行实时监测，保存数据亏部分变量名失误更正

---
+++ warehouse0707-master/Warehouse/Update/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Update
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            string path = System.Windows.Forms.Application.StartupPath;
+            FileStream fs = new FileStream(path + "\\StopRun.txt", FileMode.Create | FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(e.ExceptionObject.ToString());
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            Environment.Exit(-1);
+        }
+
+    }
+}
